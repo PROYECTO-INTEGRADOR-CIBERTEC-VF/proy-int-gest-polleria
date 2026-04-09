8de0fb153e70950e91f626603b1ef9cb9d995b6f
Carlos Gustavo Molina Zubiri <carlosgmolina1995@gmail.com>
2026-04-08 20:52:20 -0500

    Creación modelo Cliente

---
+++ gest-polleria/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace gest_polleria.Models
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string? TipoDocumento { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? RazonSocial { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+        public bool EsEmpresa { get; set; }
+    }
+}
