c2dbede0a4ff413e2eb10c8febfc4ec91c5742c3
Carlos Gustavo Molina Zubiri <carlosgmolina1995@gmail.com>
2026-04-15 18:06:07 -0500

    ClienteDAO e inicio de controlador

--- gest-polleria/Controllers/ClientesApiController.cs
+++ gest-polleria/Controllers/ClientesApiController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using gest_polleria.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using gest_polleria.DAO;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace gest_polleria.Controllers
 {
@@ -7,5 +11,16 @@ namespace gest_polleria.Controllers
     [ApiController]
     public class ClientesApiController : ControllerBase
     {
+        ClienteDAO _cliente = new ClienteDAO();
+
+        // GET: api/ClientesApi/listar?esEmpresa=true&buscar=...
+        [HttpGet("listar")]
+        public async Task<IEnumerable<Cliente>> listar([FromQuery] bool? esEmpresa = null, [FromQuery] string? buscar = null)
+        {
+            return await Task.Run(() => _cliente.listar(esEmpresa, buscar));
+        }
+
+
+
     }
 }
