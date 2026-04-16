7dc2ad75c6eec8db3825d14fe659000faedd554f
diana-medina-25 <dianamedinap25@gmail.com>
2026-04-15 19:25:27 -0500

    Creación de Modelo ConsumoInsumo

---
+++ gest-polleria/Models/ConsumoInsumo.cs
@@ -0,0 +1,8 @@
+﻿namespace gest_polleria.Models
+
+    public class ConsumoInsumo
+    {
+        public int IdInsumo { get; set; }
+        public string Insumo { get; set; } = "";
+        public decimal CantidadConsumida { get; set; }
+    }
