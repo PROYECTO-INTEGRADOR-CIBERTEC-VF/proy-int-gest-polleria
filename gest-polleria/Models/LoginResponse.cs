ca2be99076c0e58ed32a45cd44789a6b482231af
Yean Cespedes <orihaya99@gmail.com>
2026-04-22 18:25:04 -0500

    HU-06: Agrega modelos LoginRequest y LoginResponse

--- gest-polleria/Models/LoginResponse.cs
+++ gest-polleria/Models/LoginResponse.cs
@@ -5,7 +5,7 @@
         public bool Ok { get; set; }
         public string Mensaje { get; set; } = string.Empty;
         public int? IdUsuario { get; set; }
-        public string? Usuario { get; set; }
+        public string? UserName { get; set; }
         public string? NombreCompleto { get; set; }
         public string? Rol { get; set; }
     }
