d1ba974d8007d9cc6d5f749639fa506e716f6a2f
diana-medina-25 <dianamedinap25@gmail.com>
2026-04-22 19:17:39 -0500

    cree la clase AjusteInventario

---
+++ gest-polleria/Models/AjusteInventario.cs
@@ -0,0 +1,11 @@
+﻿namespace gest_polleria.Models
+
+    public class AjusteInventario
+{
+    public int IdInsumo { get; set; }
+    public decimal CantidadAjuste { get; set; }
+    public string Motivo { get; set; } = "";
+    public int IdUsuario { get; set; }
+}
+
+
