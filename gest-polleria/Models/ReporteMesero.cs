f11b360123ba0972898c1b8d51b1e136a429bd14
maicky-spec <maickygy@gmail.com>
2026-04-23 16:45:59 -0500

    Creacion reportemesero

---
+++ gest-polleria/Models/ReporteMesero.cs
@@ -0,0 +1,10 @@
+﻿namespace gest_polleria.Models
+{
+    public class ReporteMesero
+    {
+        public int IdMesero { get; set; }
+        public string Mesero { get; set; } = "";
+        public int TotalPedidos { get; set; }
+        public decimal TotalVendido { get; set; }
+    }
+}
