48f30e79802a5d80610b36ebe9b9997676fcee48
maicky-spec <maickygy@gmail.com>
2026-04-23 15:51:25 -0500

    creacion de errorviewmodel

---
+++ gest-polleria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace gest_polleria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
