1c96131fe3aba493d26bc45395ce9c927f206254
maicky-spec <maickygy@gmail.com>
2026-04-23 16:32:58 -0500

    creacion de loginrequest

---
+++ gest-polleria/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace gest_polleria.Models
+{
+    public class LoginRequest
+    {
+        public string Usuario { get; set; } = string.Empty;
+        public string Clave { get; set; } = string.Empty;
+    }
+}
