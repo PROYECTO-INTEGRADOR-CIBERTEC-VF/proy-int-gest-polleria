2e8ec67dfeb04ad99e7ce8c8c599d01fc6302dec
maicky-spec <maickygy@gmail.com>
2026-04-23 15:46:46 -0500

    Creacion de CompraDetalle

---
+++ gest-polleria/Models/CompraDetalle.cs
@@ -0,0 +1,17 @@
+﻿namespace gest_polleria.Models
+{
+    public class CompraDetalle
+    {
+        public int IdCompraDetalle { get; set; }
+
+        public int IdCompra { get; set; }
+
+        public int IdInsumo { get; set; }
+
+        public decimal Cantidad { get; set; }
+
+        public decimal CostoUnitario { get; set; }
+
+        public decimal Subtotal { get; set; }
+    }
+}
