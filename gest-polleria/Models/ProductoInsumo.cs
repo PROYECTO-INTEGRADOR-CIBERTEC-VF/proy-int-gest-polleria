83cdae26f4929648536844c515ac81a8c6707e77
diana-medina-25 <dianamedinap25@gmail.com>
2026-04-22 19:37:41 -0500

    Creacion de Modelo ProductoInsumo

---
+++ gest-polleria/Models/ProductoInsumo.cs
@@ -0,0 +1,12 @@
+﻿namespace gest_polleria.Models
+
+public class ProductoInsumo
+{
+    public int IdProductoInsumo { get; set; }
+    public int IdProducto { get; set; }
+    public int IdInsumo { get; set; }
+    public decimal Cantidad { get; set; }
+
+    public string? Insumo { get; set; }
+    public string? Abreviatura { get; set; }
+}
