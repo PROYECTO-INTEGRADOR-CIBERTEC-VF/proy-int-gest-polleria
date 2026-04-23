20bf01998c2bb98dcf6a2f7c8df8365a906cec99
maicky-spec <maickygy@gmail.com>
2026-04-23 16:39:44 -0500

    creacion de pedidos

---
+++ gest-polleria/Models/Pedidos.cs
@@ -0,0 +1,14 @@
+﻿namespace gest_polleria.Models
+{
+    public class Pedidos
+    {
+        public int IdPedido { get; set; }
+        public int IdTipoPedido { get; set; }
+        public int IdEstadoPedido { get; set; }
+        public int? IdMesa { get; set; }
+        public int? IdCliente { get; set; }
+        public DateTime FechaPedido { get; set; }
+
+        public string? DireccionDelivery { get; set; }
+    }
+}
