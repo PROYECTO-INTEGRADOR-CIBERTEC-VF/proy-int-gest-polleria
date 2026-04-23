85cd86271ce1eaab283a04295a2a69cb49b484d9
maicky-spec <maickygy@gmail.com>
2026-04-23 16:50:10 -0500

    creacion de stockreporte

---
+++ gest-polleria/Models/StockReporte.cs
@@ -0,0 +1,11 @@
+﻿namespace gest_polleria.Models
+{
+    public class StockReporte
+    {
+        public int IdInsumo { get; set; }
+        public string Insumo { get; set; } = "";
+        public decimal StockActual { get; set; }
+        public decimal StockMinimo { get; set; }
+        public string EstadoStock { get; set; } = "";
+    }
+}
