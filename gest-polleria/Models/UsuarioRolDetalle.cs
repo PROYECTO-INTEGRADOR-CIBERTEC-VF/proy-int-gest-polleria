4f3ba32859fcd6d3650fc7919152a0d6870ffdba
maicky-spec <maickygy@gmail.com>
2026-04-23 17:08:32 -0500

    creacion de usuarioroldetalle

---
+++ gest-polleria/Models/UsuarioRolDetalle.cs
@@ -0,0 +1,10 @@
+﻿namespace gest_polleria.Models
+{
+    public class UsuarioRolDetalle
+    {
+        public int IdUsuario { get; set; }
+        public int IdRol { get; set; }
+        public string NombreRol { get; set; } = "";
+        public string? Descripcion { get; set; }
+    }
+}
