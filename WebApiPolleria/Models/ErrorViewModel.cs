367949cf7e8384ae183e4bfc3f5f07fa8d5b29b5
maicky-spec <maickygy@gmail.com>
2026-04-23 18:01:48 -0500

    cambio

---
+++ WebApiPolleria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApiPolleria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
