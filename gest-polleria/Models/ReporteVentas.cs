ccaba3d3ae094d9a8576a0c1e96e47a24e301c31
maicky-spec <maickygy@gmail.com>
2026-04-23 16:47:45 -0500

    creacion de ventas

---
+++ gest-polleria/Models/ReporteVentas.cs
@@ -0,0 +1,9 @@
+﻿namespace gest_polleria.Models
+{
+    public class ReporteVentas
+    {
+        public DateTime Fecha { get; set; }
+        public int TotalPedidos { get; set; }
+        public decimal TotalVentas { get; set; }
+    }
+}
