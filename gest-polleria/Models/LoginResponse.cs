76f438f5d3d10e7eb4d75d90211f3a36cffbb225
maicky-spec <maickygy@gmail.com>
2026-04-23 16:34:20 -0500

    creacion de loginresponse

---
+++ gest-polleria/Models/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace gest_polleria.Models
+{
+    public class LoginResponse
+    {
+        public bool Ok { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+
+        public int? IdUsuario { get; set; }
+        public string? Usuario { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Rol { get; set; }
+    }
+}
