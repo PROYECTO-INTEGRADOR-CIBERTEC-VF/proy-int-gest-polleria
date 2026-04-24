7a7abb90ac748e4dca819bdf6c587ddd65b0ae38
Yean Cespedes <orihaya99@gmail.com>
2026-04-24 17:23:14 -0500

    HU-06: Agrega frontend appPolleria con login, redirección por rol y vistas Admin/Mozo

---
+++ appPolleria/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace appPolleria.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string ClaveHash { get; set; } = string.Empty;
+    }
+}
