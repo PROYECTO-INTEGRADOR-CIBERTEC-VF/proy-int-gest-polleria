262944683ce0adc81165694c69153e7555444275
maicky-spec <maickygy@gmail.com>
2026-04-29 13:26:17 -0500

    creacion de estado de mesarequest

---
+++ gest-polleria/Models/EstadoMesaRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace gest_polleria.Models
+{
+    public class EstadoMesaRequest
+    {
+        public int IdMesa { get; set; }
+        public string Estado { get; set; } = "LIBRE";
+    }
+}
