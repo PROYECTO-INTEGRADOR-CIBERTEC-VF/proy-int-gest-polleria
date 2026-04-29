1fef795b2ddddb88cec3af3dae4fcc62efeaf547
maicky-spec <maickygy@gmail.com>
2026-04-29 18:29:27 -0500

    Se crea modelo MeseroTurno

---
+++ gest-polleria/DAO/MeseroTurno.cs
@@ -0,0 +1,12 @@
+﻿namespace gest_polleria.Models
+{
+    public class MeseroTurno
+    {
+        public int IdMeseroTurno { get; set; }
+        public int IdUsuario { get; set; }
+        public string Turno { get; set; } = string.Empty;
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public bool Activo { get; set; }
+    }
+}
