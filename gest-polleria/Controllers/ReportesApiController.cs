9900aa743f6d32a672ad00c860b6fd46d1e79d3a
diana-medina-25 <dianamedinap25@gmail.com>
2026-04-29 13:58:39 -0500

    Se crea Controlador Reportes

---
+++ gest-polleria/Controllers/ReportesApiController.cs
@@ -0,0 +1,26 @@
+﻿using gest_polleria.DAO;
+using gest_polleria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gest_polleria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportesApiController : ControllerBase
+    {
+        private readonly ReporteDAO _reporte = new ReporteDAO();
+
+        [HttpGet("demanda-diaria")]
+        public async Task<IEnumerable<DemandaDiariaReporte>> demandaDiaria([FromQuery] DateTime? fecha = null)
+        {
+            var f = fecha ?? DateTime.Today;
+            return await Task.Run(() => _reporte.demandaDiaria(f));
+        }
+
+        [HttpGet("demanda-rango")]
+        public async Task<IEnumerable<DemandaDiariaReporte>> demandaRango([FromQuery] DateTime fechaInicio, [FromQuery] DateTime fechaFin)
+        {
+            return await Task.Run(() => _reporte.demandaRango(fechaInicio, fechaFin));
+        }
+    }
+}
